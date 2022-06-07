1db43ef91bb124ea2e3caf8035ae520e9b0efc4d
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2022-06-07 11:10:20 +0200

    1.Try_Calc

---
+++ src/main_console.rs
@@ -0,0 +1,23 @@
+
+pub mod stack;
+pub mod calc;
+
+use std::env;
+
+fn main() {
+    let mut args_str = String::new();
+    let mut is_silent = false;
+
+    for argument in env::args().skip(1) {
+        match argument.as_ref(){
+            "-s" | "--silent" => is_silent = true,
+            _ => args_str = args_str + " " + &argument
+        }
+    }
+
+    if(is_silent){
+        print!("{}", calc::calc(&args_str).unwrap());
+    }else{
+        println!("Result = {}",  calc::calc(&args_str).unwrap());
+    }
+}

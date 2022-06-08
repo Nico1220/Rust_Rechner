524dde3b4d1d602e08a84d1b63ef927d4574bd55
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2022-06-08 12:22:55 +0200

    Update on GUi

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,56 @@
- use eframe::{egui::CentralPanel, epi::App, run_nativ}
- use std:env
+use std::{borrow::Cow, iter::FromIterator};
 
-struct Headlines;
+use eframe::{egui::{ Color32, CtxRef, FontDefinitions, FontFamily, Hyperlink, Label, Layout, Seperator, Vec2}, epi::Storage};
+
+struct Headlines{
+    articles: Vec<NewsCardData>
+}
+
+impl Headlines {
+    fn new() -> Headlines {
+        let iter = (0..20).map(|a|NewsCardData{
+            title: format!("title{}", a),
+            desc: format!("desc{}", a),
+            url: format!("https://example.com/{}", a)
+        });
+        Headlines{
+            articles: Vec::from_iter(iter)
+        }
+    }
+    fn configure_fonts(&Self { articles }: Self)
+}
+
+struct NewsCardData{
+    title: String,
+    desc: String,
+    url: String
+}
 
 impl App for Headlines {
-    fn update(&mut self, ctx: &eframe::egui::CtxRef, frame &mut eframe::epi::Frame)
-    {
-        CentralPanel::default().show(ctx, add_contents: |ui: mut Ui| {
-            ui.label("article text");
+    fn  setup(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>, storage: Option<&dyn eframe::epi::Storage>) {
+        self.configure_fonts();
+    }  
+
+    fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
+        CentralPanel::default().show(ctx, |ui|{
+            ScrollArea::auto_sized().show(ui, |ui: |{
+                for a in &self.articles{
+                                ui.label(a.title);
+                                ui.label(a.url);
+                                ui.label(a.desc);
+                            }
+            })            
         });
     }
-    fn name(&self) -> &str{
+    fn name(&self) -> &str {
         "Headlines"
     }
 }
 
 fn main() {
-    let app:Headlines = Headlines;
-    let win_options = NativeOptins::default();
-    run_nativ(app, native_optins)
+    let app = Headlines::new();
+    let mut win_options = NativeOptins::default();
+    win_options.initial_window_size = Some(Vec2::new(540, 960))
+    run_nativ(Box::new(app), win_options);
 }
 

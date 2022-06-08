a2183ce9149b0c7ae0eb7ec2774953049b69c994
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2022-06-08 11:27:18 +0200

    analyizer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
- use eframe::{epi::App, run_nativ}
+ use eframe::{egui::CentralPanel, epi::App, run_nativ}
+ use std:env
 
 struct Headlines;
 
 impl App for Headlines {
     fn update(&mut self, ctx: &eframe::egui::CtxRef, frame &mut eframe::epi::Frame)
     {
-
+        CentralPanel::default().show(ctx, add_contents: |ui: mut Ui| {
+            ui.label("article text");
+        });
+    }
+    fn name(&self) -> &str{
+        "Headlines"
     }
 }
+
 fn main() {
+    let app:Headlines = Headlines;
+    let win_options = NativeOptins::default();
     run_nativ(app, native_optins)
 }
 

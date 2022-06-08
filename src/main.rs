b35e3a7b86cafab34bce7c67fa2c7ee66eca3e66
Nico1220 <86952580+Nico1220@users.noreply.github.com>
2022-06-08 19:01:41 +0200

    gtk als neue Libary

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,25 @@
-use std::{borrow::Cow, iter::FromIterator};
+use libadwaita::{prelude::{ApplicationExt, AppInfoExtManual, WidgetExt},
+Application, ApplicationWindow,
+};
 
-use eframe::{egui::{ Color32, CtxRef, FontDefinitions, FontFamily, Hyperlink, Label, Layout, Seperator, Vec2}, epi::Storage};
-
-struct Headlines{
-    articles: Vec<NewsCardData>
+fn build_ui(application: &Application) {
+    let content = libadwaita::gtk::Box::new(Orientation::Vertical, 0);
+    content.append(&HeaderBar::builder()
+        .title_widget(&WindowTitle::new("My GTK4 RSS Reader", ""))
+        .build());
+    let window = ApplicationWindow::builder()
+        .application(application)
+        .title("My GTK4 RSS Reader")
+        .default_height(250)
+        .default_width(400)
+        .content(&content)
+        .build();
+    window.show();
 }
 
-impl Headlines {
-    fn new() -> Headlines {
-        let iter = (0..20).map(|a|NewsCardData{
-            title: format!("title{}", a),
-            desc: format!("desc{}", a),
-            url: format!("https://example.com/{}", a)
-        });
-        Headlines{
-            articles: Vec::from_iter(iter)
-        }
-    }
-    fn configure_fonts(&Self { articles }: Self)
+pub fn main() {
+    let application = Application::new(Some("com.example.gtk-rss-reader"), Default::default());
+    
+    application.connect_activate(build_ui);
+    application.run();
 }
-
-struct NewsCardData{
-    title: String,
-    desc: String,
-    url: String
-}
-
-impl App for Headlines {
-    fn  setup(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>, storage: Option<&dyn eframe::epi::Storage>) {
-        self.configure_fonts();
-    }  
-
-    fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
-        CentralPanel::default().show(ctx, |ui|{
-            ScrollArea::auto_sized().show(ui, |ui: |{
-                for a in &self.articles{
-                                ui.label(a.title);
-                                ui.label(a.url);
-                                ui.label(a.desc);
-                            }
-            })            
-        });
-    }
-    fn name(&self) -> &str {
-        "Headlines"
-    }
-}
-
-fn main() {
-    let app = Headlines::new();
-    let mut win_options = NativeOptins::default();
-    win_options.initial_window_size = Some(Vec2::new(540, 960))
-    run_nativ(Box::new(app), win_options);
-}
-
